ce6943e9d6fee6bf28ca99a9afb2bf6495aa9f35
evansitx <evansal@ext.inditex.com>
2024-10-31 12:59:46 +0100

    Testing campaign form

--- src/components/FileForm.jsx
+++ src/components/FileForm.jsx
@@ -1,69 +1,88 @@
-import { useState } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 import { Button, Input } from "@nextui-org/react";
 
 const FileForm = () => {
-  const [emailData, setEmailData] = useState({
-    attachments: "",
+  const validFileExtensions = {
+    image: ["jpg", "gif", "png", "jpeg", "svg", "webp"],
+  };
+
+  const isValidFileType = (fileName, fileType) => {
+    const extension = fileName?.split(".").pop().toLowerCase();
+    return extension && validFileExtensions[fileType].includes(extension);
+  };
+
+  const validationSchema = Yup.object().shape({
+    images: Yup.array()
+      .required("Añade al menos un archivo")
+      .test("are-valid-types", "Algunos archivos no son válidos", (value) => {
+        if (!value) return false;
+
+        return value.every((file) => isValidFileType(file.name, "image"));
+      }),
   });
 
-  const handleChange = (e) => {
-    if (e.target.name === "attachments") {
-      const files = e.target.files;
-      setEmailData({
-        ...emailData,
-        attachments: files,
-      });
-    } else {
-      setEmailData({
-        ...emailData,
-        [e.target.name]: e.target.value,
-      });
-    }
+  const handleFileChange = (event, setFieldValue) => {
+    const files = Array.from(event.currentTarget.files); // Convierte FileList a Array
+    console.log("Archivos seleccionados:", files); // Muestra los archivos en la consola
+    setFieldValue("images", files); // Actualiza el estado de Formik
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const onSubmit = (values, { resetForm }) => {
+    console.log("Formulario enviado con valores:", values);
 
-    try {
-      const formData = new FormData();
-      formData.append("attachment", emailData.attachments[0]); // Cambiado a "attachment"
-
-      const response = await fetch("http://localhost:3001/send-email", {
-        method: "POST",
-        body: formData,
-      });
-
-      if (response.ok) {
-        console.log("Correo enviado exitosamente");
-      } else {
-        console.error("Error al enviar el correo", response);
-      }
-    } catch (error) {
-      console.error("Error de red:", error);
-    }
+    resetForm();
   };
+
   return (
-    <div>
-      <div className="mb-5">
-        <h2>Enviar archivos de fotos</h2>
-      </div>
-      <div className="mt-5">
-        <form onSubmit={handleSubmit}>
+    <Formik
+      initialValues={{ images: [] }}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {({
+        setFieldValue,
+        handleSubmit,
+        errors,
+        touched,
+        isSubmitting,
+        values,
+      }) => (
+        <>
           <Input
-            label="Adjuntar Archivos"
             type="file"
-            name="attachments"
-            onChange={handleChange}
+            label="Selecciona una carpeta"
+            variant="bordered"
+            isInvalid={errors.images && touched.images}
+            errorMessage={errors.images && touched.images ? errors.images : ""}
+            className="max-w-xs"
+            onChange={(event) => handleFileChange(event, setFieldValue)}
+            name="images"
+            webkitdirectory="true"
             multiple
           />
+          {console.log("valores:", values)}
+          {values.images && values.images.length > 0 && (
+            <ul>
+              {values.images.map((value, index) => (
+                <li key={index}>{value.name}</li> // Solo se mostrarán las imágenes
+              ))}
+            </ul>
+          )}
 
-          <Button color="primary" type="submit" className="mt-5">
-            Procesar
+          {values?.image?.name && values.image.name}
+
+          <Button
+            color="primary"
+            onClick={handleSubmit}
+            isLoading={isSubmitting}
+            disabled={isSubmitting}
+          >
+            Procesar imágenes
           </Button>
-        </form>
-      </div>
-      <div></div>
-    </div>
+        </>
+      )}
+    </Formik>
   );
 };
 
--- src/pages/Campaign.jsx
+++ src/pages/Campaign.jsx
@@ -1,16 +1,9 @@
-import { Input, Spinner } from "@nextui-org/react";
-import { useFetch } from "../hooks/useFetch";
-import { useState } from "react";
 import FileForm from "../components/FileForm";
 
 const Campaign = () => {
-  const { data, loading, error } = useFetch(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-
   return (
     <>
-     <FileForm/>
+      <FileForm />
     </>
   );
 };
